e21b83001ed0e981cfcfb6d1a2c7332691ade3f5
Alexander Freyr Sveinsson <harryfreyr@gmail.com>
2019-01-28 21:03:50 +0000

    add canvas

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Canvas from './components/Canvas';
 
 class App extends Component {
   render() {
     return (
       <div className="App">        
-          <h1>
-            {this.props.message}
-          </h1>        
+          <Canvas />      
       </div>
     );
   }
---
+++ src/components/Canvas.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Canvas = () => {
+    const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight];
+    return (
+    <svg
+      id="aliens-go-home-canvas"
+      preserveAspectRatio="xMaxYMax none"    
+      viewBox={viewBox}
+    >
+      <circle cx={0} cy={0} r={50} />
+    </svg>
+  );
+};
+
+export default Canvas;
