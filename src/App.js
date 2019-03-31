71d4134b18fa1ecea511fe3f66ec40d7b859263f
unknown <harryfreyr@gmail.com>
2019-03-31 14:23:48 +0000

    spwn enemys and add start screen

--- src/App.js
+++ src/App.js
@@ -9,6 +9,12 @@ class App extends Component {
     setInterval(() => {
         self.props.moveObjects(self.canvasMousePosition);
     }, 10);
+    window.onresize = () => {
+      const cnv = document.getElementById('aliens-go-home-canvas');
+      cnv.style.width = `${window.innerWidth}px`;
+      cnv.style.height = `${window.innerHeight}px`;
+    };
+    window.onresize();
   }
 
   trackMouse(event) {
@@ -19,15 +25,31 @@ class App extends Component {
     return (
       <Canvas
         angle={this.props.angle}
+        gameState={this.props.gameState}
+        startGame={this.props.startGame}
         trackMouse={event => (this.trackMouse(event))}
       />
     );
   }
 }
 
-App.PropTypes = {
+App.propTypes = {
   angle: PropTypes.number.isRequired,
+  gameState: PropTypes.shape({
+    started: PropTypes.bool.isRequired,
+    kills: PropTypes.number.isRequired,
+    lives: PropTypes.number.isRequired,
+  }).isRequired,
+  flyingObjects: PropTypes.arrayOf(PropTypes.shape({
+    position: PropTypes.shape({
+      x: PropTypes.number.isRequired,
+      y: PropTypes.number.isRequired
+    }).isRequired,
+    id: PropTypes.number.isRequired,
+  })).isRequired,
   moveObjects: PropTypes.func.isRequired,
+  startGame: PropTypes.func.isRequired,
 };
+
 
 export default App;
